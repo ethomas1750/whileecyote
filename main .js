d7f74e8b3c248cbedd9651c5ce61d7ab8bd47391
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-07 00:18:23 -0500

    Update main .js

--- main .js
+++ main .js
@@ -1,6 +1,29 @@
 function numberDoubler(num) {
     // Your code here
     while(num <= 100){
-        num = num *2
+        num *= 2
     }
     return num
+    
+function stringRepeater(str) {
+let str2 = ""
+while(str2.length < 10){
+  str2 = str + str2
+}
+return str2
+}
+
+
+function makeDivisible(x, y) {
+
+while(x % y !== 0){
+    x = x + 1
+}
+return x
+}
+
+module.exports = {
+    numberDoubler,
+    stringRepeater,
+    makeDivisible
+};
